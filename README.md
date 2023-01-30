62c3d4e929891bb5e0de4cbc661cdd7e5e3cdf8f
pentonvillefandango <118852278+pentonvillefandango@users.noreply.github.com>
2023-01-30 08:53:29 +0000

    added descriptions

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # acmfirewall
+
+v0.1
+Initial commit
+
+v0.2
+Basic Functionality
+acmfirewall -b <path/to/file> blocks CIDRS in file
+acmfirewall -u <path/to/file> unblocks CIDRs in file
+acmfirewall -l <path/to/file> lists blocked CIDRs
---
+++ acmfire.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This script will Interrogate/Activate/Deactviate the acm firewall for one or more IPs / CIDR
+# -v verbose -h help -f file -l log -b block -u unblock - i interogate
+
+
+# Check user is root
+
+
+# process arguments
+
+
+
+
+# usage function
+
+
+
+# log function
+
+
+
+# help function
+
+
+# dtelegy function
+
+
+# report funntion
+
