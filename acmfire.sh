26eef3f41c8b6f709309b286081ce701deeef711
pentonvillefandango <118852278+pentonvillefandango@users.noreply.github.com>
2023-01-30 15:21:14 +0000

    v.01 changes

--- acmfire.sh
+++ acmfire.sh
@@ -5,7 +5,7 @@
 # This script will Interrogate/Activate/Deactviate the acm firewall for one or more IPs / CIDR
 # -v verbose -h help -f file -l log -b block -u unblock - i interogate -h header
 # WARNING - not everything above is implemented yet. See README.md for details of this version
-
+# process arguments
 
 # Check user is root
 if [[ ${UID} -ne 0 ]]
